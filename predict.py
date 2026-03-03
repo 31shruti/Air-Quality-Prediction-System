80a28a7efc7d603ff41ca7f74113ce16f530a644
31shruti <31shrutisoni@gmail.com>
2026-03-03 15:46:02 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,12 +1,19 @@
+#-----AQI Model Testing Script-----
 import numpy as np
 import joblib
 from tensorflow.keras.models import load_model # type: ignore
 
 print ("Loading model and scaler...")
+
+# Loading trained LSTM model
 model = load_model("aqi_lstm_model.h5", compile=False)
+
+# Loading saved MinMax scaler
 scaler = joblib.load("scaler.save")
 
 print("Model loaded successfully.")
+
+#-----Creating Sample Input for Testing-----
 
 # Example input (last 24 hours data simulation)
 # You must provide 24 rows with 7 features
@@ -14,22 +21,29 @@ print("Model loaded successfully.")
 
 sample_input = np.array([
     [200, 8.0, 95, 5.0, 150, 160, 995]
-] * 24)
+] * 24)  # Repeating same values for 24 hours
 
-# Scale input
+#-----Scaling Input Data-----
+
+# Scaling input using previously fitted scaler
 scaled_input = scaler.transform(sample_input)
 
-# Reshape for LSTM (1 sample, 24 time steps, 7 features)
+# Reshaping data to match LSTM expected input shape
+# Shape required: (samples, time_steps, features)
 scaled_input = np.reshape(scaled_input, (1, 24, 7))
 
 print("Making prediction...")
 
+
+# Predicting next hour AQI (scaled value)
 prediction = model.predict(scaled_input)
 
-# Convert prediction back
+#-----Converting Prediction Back to Real Value-----
+
+# Creating dummy array for inverse scaling
 dummy = np.zeros((1, 7))
 dummy[:, 0] = prediction[:, 0]
 
+# Getting actual AQI value
 predicted_aqi = scaler.inverse_transform(dummy)[:, 0]
-
 print("Predicted AQI:", predicted_aqi[0])
