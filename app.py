6ec0e043e769163eb1943ff187aa942c15c298f0
31shruti <31shrutisoni@gmail.com>
2026-03-03 14:37:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Importing libraries
 import streamlit as st # type: ignore
 import numpy as np
 import pandas as pd
@@ -5,6 +6,7 @@ import joblib
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model # type: ignore
 
+# Function to classify AQI into categories based on standard AQI ranges
 def get_aqi_category(aqi):
     if aqi <= 50:
         return "Good"
@@ -18,34 +20,41 @@ def get_aqi_category(aqi):
         return "Very Unhealthy"
     else:
         return "Hazardous"
-    
+
+# Setting page configuration for Streamlit app    
 st.set_page_config(page_title="AQI Forecast System", layout="centered")
 
+# Title of the project
 st.title("AQI Forecasting (Next Hour Prediction)")
-
 st.write("Upload last 24 hours air quality data (CSV file).")
 
-# Load model and scaler
+# Loading trained LSTM model and scaler
 model = load_model("aqi_lstm_model.h5", compile=False)
 scaler = joblib.load("scaler.save")
 
+# Will help to upload CSV file from user
 uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
 
 if uploaded_file is not None:
     
+    # Reading uploaded file into pandas dataframe
     data = pd.read_csv(uploaded_file)
+    # Initializing prediction variables
     predicted_aqi = None
     lower_bound = None
     upper_bound = None
 
+    # Displaying first few rows to verify data 
     st.write("Preview of uploaded data:")
     st.write(data.head())
-
+    
+     # This will help to check if user uploaded exactly 24 hours data
     if len(data) != 24:
         st.error("Please upload exactly 24 rows (24 hours data).")
 
     else:
-
+        
+        # This will Button to trigger prediction
         if st.button("Predict Next Hour AQI"):
 
             scaled_data = scaler.transform(data)
@@ -56,12 +65,14 @@ if uploaded_file is not None:
             dummy[0, 0] = prediction[0][0]
 
             predicted_aqi = scaler.inverse_transform(dummy)[0][0]
+            # Getting AQI category
             category = get_aqi_category(predicted_aqi)
 
             confidence_margin = 5
             lower_bound = predicted_aqi - confidence_margin
             upper_bound = predicted_aqi + confidence_margin
 
+            # Displaying results
             st.success(f"Predicted Next Hour AQI: {round(predicted_aqi, 2)}")
             st.info(f"AQI Category: {category}")
             st.write(f"Prediction Range: {round(lower_bound,2)} - {round(upper_bound,2)}")
@@ -69,15 +80,19 @@ if uploaded_file is not None:
         # -------- GRAPH SECTION --------
         st.subheader("Last 24 Hours AQI Trend + Forecast")
 
+        # Creating new plot
         plt.figure()
 
         hours = list(range(24))
         future_hour = 24
 
+        # Plotting last 24 hours AQI trend
         plt.plot(hours, data["aqi_index"].values, marker='o')
 
+        # If prediction is available, it will plot forecast
         if predicted_aqi is not None:
 
+            # Drawing dashed line from last hour to predicted hour
             plt.plot(
                 [23, future_hour],
                 [data["aqi_index"].values[-1], predicted_aqi],
@@ -95,8 +110,10 @@ if uploaded_file is not None:
                 f"{round(predicted_aqi, 2)}"
             )
 
+         # Labeling graph axes
         plt.xlabel("Hour")
         plt.ylabel("AQI")
         plt.xticks(range(0, 25))
 
+        # Showing plot in Streamlit
         st.pyplot(plt)
